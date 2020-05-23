cf96acb6cc239395a4029c3251ea8dd0c23983b9
Chisa Yomoda <h4n1virus@gmail.com>
2020-05-23 07:27:21 +0300

    package shit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SMSPVA wrapper
+
+SMSPVA WRAPPER.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smspva-wrapper",
+    version="0.0.1",
+    author="Terry",
+    author_email="terry@beatwo.men",
+    description="SMSPVA wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/h4n1virus/smspva-wrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: WTFPL License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
